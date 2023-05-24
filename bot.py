e03fe4cea83800fb9bc409e5faf80b7cb604a564
Denys <79135422+Gr1nchikJ@users.noreply.github.com>
2023-05-24 22:05:33 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,4 +4,4 @@ TOKEN = "6029933916:AAHQdPGxZVJYVI8rM6GmeuwzWkC5KQ0575U"
 
 bot = telegram.Bot(TOKEN)
 
-bot.send_message(chat_id = '394520158', text="Hello world")
+bot.send_message(chat_id = '357626127', text="Hello world")
