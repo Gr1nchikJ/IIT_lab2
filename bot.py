caeb537a1aec7e1ae6d9ed2ce21fc910c61af389
Denys <79135422+Gr1nchikJ@users.noreply.github.com>
2023-05-24 22:25:36 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import telegram
 
-TOKEN = "6029933916:AAHQdPGxZVJYVI8rM6GmeuwzWkC5KQ0575U"
+TOKEN = '6029933916:AAHQdPGxZVJYVI8rM6GmeuwzWkC5KQ0575U'
 
 bot = telegram.Bot(TOKEN)
 
-await bot.send_message(chat_id='357626127', text='Hello world')
+await bot.send_message(chat_id='357626127', text="Hello world")
